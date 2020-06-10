a68b7a5dfbaa4cb8059e45db38a2ffbee0b246c4
Jose Salvatierra <jslvtr@gmail.com>
2020-06-10 11:56:06 +0100

    First commit.

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, url_for, session, request
+
+app = Flask(__name__)
+app.secret_key = "jose"
+
+
+@app.route("/", methods=["GET", "POST"])
+def setup():
+    if request.method == "POST":
+        exercise = int(request.form["exercise"])
+        rest = int(request.form["rest"])
+        sets = int(request.form["sets"])
+
+        session["exercise"] = exercise
+        session["rest"] = rest
+        session["sets"] = sets
+        session["set_counter"] = 0
+
+        return redirect(url_for("rest"))
+    return render_template("home.jinja2")
+
+
+@app.route("/rest")
+def rest():
+    return render_template("rest.jinja2", rest=session["rest"], sets=session["set_counter"])
+
+
+@app.route("/exercise")
+def exercise():
+    session["set_counter"] += 1
+    if session["set_counter"] == session["sets"] + 1:
+        return redirect(url_for("completed"))
+    return render_template("exercise.jinja2", exercise=session["exercise"], sets=session["set_counter"])
+
+
+@app.route("/complete")
+def completed():
+    return render_template("complete.jinja2", sets=session["set_counter"])
